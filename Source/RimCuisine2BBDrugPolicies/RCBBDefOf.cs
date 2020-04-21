822c591541d3eb40faa71b6c511e37562d594054
Mlie <emipa606@users.noreply.github.com>
2020-04-21 22:11:01 +0200

    First publish

---
+++ Source/RimCuisine2BBDrugPolicies/RCBBDefOf.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using RimWorld;
+using Verse;
+
+namespace RimCuisineBBDrugPolicies
+{
+	// Token: 0x02000003 RID: 3
+	[DefOf]
+	public static class RCBBDefOf
+	{
+		// Token: 0x04000001 RID: 1
+		public static ThingDef RC2_Ale;
+
+		// Token: 0x04000002 RID: 2
+		public static ThingDef RC2_Brandy;
+
+		// Token: 0x04000003 RID: 3
+		public static ThingDef RC2_Cider;
+
+		// Token: 0x04000004 RID: 4
+		public static ThingDef RC2_FruitWine;
+
+		// Token: 0x04000005 RID: 5
+		public static ThingDef RC2_Gin;
+
+		// Token: 0x04000006 RID: 6
+		public static ThingDef RC2_Mead;
+
+		// Token: 0x04000007 RID: 7
+		public static ThingDef RC2_Moonshine;
+
+		// Token: 0x04000008 RID: 8
+		public static ThingDef RC2_Rum;
+
+		// Token: 0x04000009 RID: 9
+		public static ThingDef RC2_Sake;
+
+		// Token: 0x0400000A RID: 10
+		public static ThingDef RC2_Stout;
+
+		// Token: 0x0400000B RID: 11
+		public static ThingDef RC2_Tequila;
+
+		// Token: 0x0400000C RID: 12
+		public static ThingDef RC2_Vodka;
+
+		// Token: 0x0400000D RID: 13
+		public static ThingDef RC2_Whiskey;
+
+		// Token: 0x0400000E RID: 14
+		public static ThingDef RC2_Wine;
+
+		// Token: 0x0400000F RID: 15
+		public static ThingDef RC2_Arrack;
+
+		// Token: 0x04000010 RID: 16
+		public static ThingDef RC2_Grog;
+
+		// Token: 0x04000011 RID: 17
+		public static ThingDef RC2_Kumis;
+	}
+}
